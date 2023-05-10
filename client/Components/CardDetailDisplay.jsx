b20c9e834450a61320d1f56d5d5d440ddc2e0433
Weston Clark <westoclark@gmail.com>
2023-05-09 18:21:32 -0700

    display person info when box is clicked in tree view

--- client/Components/CardDetailDisplay.jsx
+++ client/Components/CardDetailDisplay.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 
-export default function () {
-  const { first_name, last_name, birthday, death_date, email, phone_number, street_address, city, state, zip_code, mom_id, dad_id, spouse_id } = props.personInfo;
-
+export default function (props) {
+  // const { first_name, last_name, birthday, death_date, email, phone_number, street_address, city, state, zip_code, mom_id, dad_id, spouse_id } = props.personInfo;
+  const { mom, dad, spouse, children, person } = props;
+  console.log('person object from card details', person.person.first_name);
   return (
-    <div style={{ width: '180px', height: '80px', border: '1px solid black', margin: '10px', padding: '5px'}}>
-      <p>Name: {first_name} {last_name}</p>
-      <p>Birthday: {birthday.slice(0, 10)} - {death_date.slice(0, 10)}</p>
-      <p>Phone Number: {phone_number} </p>  
-      <p>Email: {email} </p>  
-      <p>Address: {street_address}, {city}, {state} {zip_code} </p>  
-      <p>Spouse: <button> spouse </button> 
-      </p>  
-      <p>Mom: <button> mom </button> </p>  
-      <p>Dad: <button> dad </button>  </p>  
-
+    <div style={{ width: '180px', height: '280px', border: '1px solid black', margin: '10px', padding: '5px' }}>
+      <p>
+        Name: {person.person.first_name} {person.person.last_name}
+      </p>
+      {/* <p>
+        Birthday: {person.person.birthday.slice(0, 10)} - {person.person.death_date.slice(0, 10)}
+      </p>
+      <p>Phone Number: {phone_number} </p>
+      <p>Email: {email} </p>
+      <p>
+        Address: {street_address}, {city}, {state} {zip_code}{' '}
+      </p>
+      <p>
+        Spouse: <button> spouse </button>
+      </p>
+      <p>
+        Mom: <button> {mom.first_name} </button>{' '}
+      </p>
+      <p>
+        Dad: <button> dad </button>{' '}
+      </p> */}
     </div>
   );
-
-};
+}

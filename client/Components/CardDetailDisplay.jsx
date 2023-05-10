ae9733e4a32852f4dbd6375a1bef55523d10a3a6
Weston Clark <westoclark@gmail.com>
2023-05-09 18:27:14 -0700

    more card details added

--- client/Components/CardDetailDisplay.jsx
+++ client/Components/CardDetailDisplay.jsx
@@ -3,28 +3,43 @@ import React from 'react';
 export default function (props) {
   // const { first_name, last_name, birthday, death_date, email, phone_number, street_address, city, state, zip_code, mom_id, dad_id, spouse_id } = props.personInfo;
   const { mom, dad, spouse, children, person } = props;
-  console.log('person object from card details', person.person.first_name);
+  const { first_name, last_name, birthday, death_date, email, phone_number, street_address, city, state, zip_code, mom_id, dad_id, spouse_id } = person.person;
   return (
-    <div style={{ width: '180px', height: '280px', border: '1px solid black', margin: '10px', padding: '5px' }}>
+    <div style={{ width: '200px', height: '280px', border: '1px solid black', margin: '10px', padding: '5px' }}>
       <p>
-        Name: {person.person.first_name} {person.person.last_name}
+        Name: {first_name} {last_name}
       </p>
-      {/* <p>
-        Birthday: {person.person.birthday.slice(0, 10)} - {person.person.death_date.slice(0, 10)}
+      <p>
+        Birthday: {birthday.slice(0, 10)} 
+      </p>
+      <p>
+        Death Date: {death_date.slice(0, 10)}
       </p>
       <p>Phone Number: {phone_number} </p>
       <p>Email: {email} </p>
       <p>
         Address: {street_address}, {city}, {state} {zip_code}{' '}
       </p>
-      <p>
-        Spouse: <button> spouse </button>
+      {/* <p>
+        Spouse:{' '}
+        <button>
+          {' '}
+          {spouse ? spouse.first_name : 'add spouse'} {spouse.last_name}{' '}
+        </button>
       </p>
       <p>
-        Mom: <button> {mom.first_name} </button>{' '}
+        Mom:{' '}
+        <button>
+          {' '}
+          {mom.first_name} {mom.last_name}{' '}
+        </button>{' '}
       </p>
       <p>
-        Dad: <button> dad </button>{' '}
+        Dad:{' '}
+        <button>
+          {' '}
+          {dad.first_name} {dad.last_name}{' '}
+        </button>{' '}
       </p> */}
     </div>
   );

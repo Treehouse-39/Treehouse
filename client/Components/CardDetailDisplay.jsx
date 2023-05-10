c60e144f7458363855c60e2c3afbbd23357bfda9
Krystal Fung <krystalfung@krystals-mbp.mynetworksettings.com>
2023-05-09 22:50:51 -0400

    components will switch between card and tree view. Still need to make the rest of the buttons functional

--- client/Components/CardDetailDisplay.jsx
+++ client/Components/CardDetailDisplay.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 export default function (props) {
   // const { first_name, last_name, birthday, death_date, email, phone_number, street_address, city, state, zip_code, mom_id, dad_id, spouse_id } = props.personInfo;
-  const { mom, dad, spouse, children, person } = props;
+  const { person, setViewTree } = props;
+  const { mom, dad, spouse, children } = person;
   const { first_name, last_name, birthday, death_date, email, phone_number, street_address, city, state, zip_code, mom_id, dad_id, spouse_id } = person.person;
+  console.log(person)
   return (
-    <div style={{ width: '200px', height: '280px', border: '1px solid black', margin: '10px', padding: '5px' }}>
+    <div style={{ width: '200px', height: '600px', border: '1px solid black', margin: '10px', padding: '5px', borderRadius: '5px', backgroundColor: 'lightcyan'}}>
+      <button style={{marginLeft: '150px', width: '50px'}} onClick={() => { setViewTree(true) }}> Back </button>
       <p>
         Name: {first_name} {last_name}
       </p>
@@ -20,27 +24,23 @@ export default function (props) {
       <p>
         Address: {street_address}, {city}, {state} {zip_code}{' '}
       </p>
-      {/* <p>
+      <p>
         Spouse:{' '}
-        <button>
-          {' '}
-          {spouse ? spouse.first_name : 'add spouse'} {spouse.last_name}{' '}
-        </button>
+        { spouse ? <button style={{margin: '2px 5px'}} > {' '} {spouse.first_name} {spouse.last_name}{' '} </button> : <button style={{margin: '2px 5px'}} > Add Spouse </button> }
       </p>
       <p>
         Mom:{' '}
-        <button>
-          {' '}
-          {mom.first_name} {mom.last_name}{' '}
-        </button>{' '}
+        { mom ? <button style={{margin: '2px 5px'}} > {' '} {mom.first_name} {mom.last_name}{' '} </button> : <button style={{margin: '2px 5px'}} > Add Mom </button> }
       </p>
       <p>
         Dad:{' '}
-        <button>
-          {' '}
-          {dad.first_name} {dad.last_name}{' '}
-        </button>{' '}
-      </p> */}
+        { dad ? <button style={{margin: '2px 5px'}} > {' '} {dad.first_name} {dad.last_name}{' '} </button> : <button style={{margin: '2px 5px'}} > Add Dad </button> }
+      </p>
+      {/* // need to iterate over children array to add children to card */}
+      <p>
+        Children: 
+        { children ? <button style={{margin: '2px 5px'}} > {' '} {children.first_name} {children.last_name}{' '} </button> : <button style={{margin: '2px 5px'}} > Add Child </button> }
+      </p>
     </div>
   );
 }
--- client/Components/PersonCard.jsx
+++ client/Components/PersonCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default function PersonCard(props) {
-  const { first_name, last_name, birthday, death_date } = props.personInfo;
+  const { first_name, last_name, sex, birthday, death_date } = props.personInfo;
   const { getDetails } = props;
   return (
     <>
+    {/* style={{ (sex === 'male') ? {backgroundColor: 'lightblue'} : {backgroundColor: 'lightpink'} }}; */}
       <button
-        style={{ width: '180px', height: '80px', border: '1px solid black', margin: '10px', padding: '5px' }}
+        style={{ width: '180px', height: '80px', border: '1px solid black', margin: '10px', padding: '5px', borderRadius: '5px', backgroundColor: 'lightgreen' }}
         onClick={async () => {
           console.log('clicked');
           await getDetails(first_name, last_name, birthday);
